e382b8c3fd31ddb26f52b4aa4ab084dab09d3ede
Georges <georges.aguerre@wanadoo.fr>
2025-03-04 16:48:33 +0100

    Version du composant avec 2 boutons

--- force-app/main/default/lwc/tableauExpedition/tableauExpedition.js
+++ force-app/main/default/lwc/tableauExpedition/tableauExpedition.js
@@ -4,8 +4,24 @@ import getLivraisons from '@salesforce/apex/LivraisonController.getLivraisons';
 const COLUMNS = [
     { label: 'Numéro de Livraison', fieldName: 'Name', type: 'text' },
     { label: 'Pays de Destination', fieldName: 'PaysDestination__c', type: 'text' },
-    { label: 'État', fieldName: 'Etat__c', type: 'text' },
-    { label: 'Date de Livraison', fieldName: 'DateLivraison__c', type: 'date' }
+    {
+        label: 'Meilleur Prix',
+        type: 'button-icon',
+        typeAttributes: {
+            iconName: 'utility:moneybag',
+            variant: 'border-filled',
+            alternativeText: 'Dollar'
+        }
+    },
+    {
+        label: 'Meilleur Delai',
+        type: 'button-icon',
+        typeAttributes: {
+            iconName: 'utility:reminder',
+            variant: 'border-filled',
+            alternativeText: 'Camion'
+        }
+    }
 ];
 
 export default class TableauExpedition extends LightningElement {

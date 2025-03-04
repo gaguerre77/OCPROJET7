2e81d8e0c2b86e88c30633d28b23503d55e41a96
Georges <georges.aguerre@wanadoo.fr>
2025-03-04 19:05:50 +0100

    Mise en place des methodes pour completer l'enregistrement

--- force-app/main/default/lwc/tableauExpedition/tableauExpedition.js
+++ force-app/main/default/lwc/tableauExpedition/tableauExpedition.js
@@ -1,5 +1,7 @@
 import { LightningElement, wire } from 'lwc';
 import getLivraisons from '@salesforce/apex/LivraisonController.getLivraisons';
+import completeLivraisonMeilleurPrix from '@salesforce/apex/LivraisonController.completeLivraisonMeilleurPrix';
+import completeLivraisonMeilleurDelai from '@salesforce/apex/LivraisonController.completeLivraisonMeilleurDelai';
 
 const COLUMNS = [
     { label: 'Numéro de Livraison', fieldName: 'Name', type: 'text' },
@@ -10,7 +12,9 @@ const COLUMNS = [
         typeAttributes: {
             iconName: 'utility:moneybag',
             variant: 'border-filled',
-            alternativeText: 'Dollar'
+            alternativeText: 'Dollar',
+            name: 'meilleur_prix',
+            title: 'Compléter avec le meilleur prix'
         }
     },
     {
@@ -19,7 +23,9 @@ const COLUMNS = [
         typeAttributes: {
             iconName: 'utility:reminder',
             variant: 'border-filled',
-            alternativeText: 'Camion'
+            alternativeText: 'Camion',
+            name: 'meilleur_delai',
+            title: 'Compléter avec le meilleur délai'
         }
     }
 ];
@@ -35,6 +41,52 @@ export default class TableauExpedition extends LightningElement {
         } else if (error) {
             this.showError(error);
         }
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+
+        switch (actionName) {
+            case 'meilleur_prix':
+                this.completeLivraisonMeilleurPrix(row.Id);
+                break;
+            case 'meilleur_delai':
+                this.completeLivraisonMeilleurDelai(row.Id);
+                break;
+            default:
+                this.showError(`Action inconnue : ${actionName}`);
+        }
+    }
+
+    completeLivraisonMeilleurPrix(livraisonId) {
+        completeLivraisonMeilleurPrix({ livraisonId })
+            .then(() => {
+                this.refreshLivraisons();
+            })
+            .catch(error => {
+                this.showError(error);
+            });
+    }
+
+    completeLivraisonMeilleurDelai(livraisonId) {
+        completeLivraisonMeilleurDelai({ livraisonId })
+            .then(() => {
+                this.refreshLivraisons();
+            })
+            .catch(error => {
+                this.showError(error);
+            });
+    }
+
+    refreshLivraisons() {
+        return getLivraisons()
+            .then(result => {
+                this.livraisons = result;
+            })
+            .catch(error => {
+                this.showError(error);
+            });
     }
 
     showError(error) {
